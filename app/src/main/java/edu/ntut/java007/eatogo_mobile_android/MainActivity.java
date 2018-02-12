b677eeac9afa81a5835a2700c23df3fcd8f32750
idontwannarock <idontwannarock@gmail.com>
2018-02-12 22:59:42 +0800

    Initialized Android project.

---
+++ app/src/main/java/edu/ntut/java007/eatogo_mobile_android/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.ntut.java007.eatogo_mobile_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
